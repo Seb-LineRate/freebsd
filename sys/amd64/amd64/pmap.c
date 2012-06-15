3cbb7d51eb04412bd6cc8eca7c0cca08c90a8678
Sebastian Kuzminsky <seb@lineratesystems.com>
2012-06-15 09:57:38 -0600

    add a new pagesize (1gb)

--- sys/amd64/amd64/pmap.c
+++ sys/amd64/amd64/pmap.c
@@ -798,6 +798,11 @@ pmap_init(void)
 		KASSERT(MAXPAGESIZES > 1 && pagesizes[1] == 0,
 		    ("pmap_init: can't assign to pagesizes[1]"));
 		pagesizes[1] = NBPDR;
+		KASSERT(MAXPAGESIZES > 2 && pagesizes[2] == 0,
+		    ("pmap_init: can't assign to pagesizes[2]"));
+		if ((amd_feature & AMDID_PAGE1GB) == AMDID_PAGE1GB) {
+			pagesizes[2] = NBPDP;
+		}
 	}
 
 	/*
